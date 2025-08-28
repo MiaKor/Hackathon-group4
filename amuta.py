8207b92206e86b0b64dec0077ee87642503e919f
ptamar <ptamar@post.bgu.ac.il>
2025-08-28 13:32:08 +0300

    yael

---
+++ amuta.py
@@ -0,0 +1,31 @@
+import consts
+
+from screen import draw_message
+
+
+def amuta(name, my_data):
+    my_amuta_data = my_data[name]
+
+    location = (0, 100)
+
+    for i in my_amuta_data:
+
+        location = (0, location[1])
+
+        for k in i:
+
+            draw_message(k, 40, "black", location)
+
+            location = (location[0] + 200, location[1])
+
+            if i.index(k) == len(i) - 1:
+                location = (location[0], location[1] + 200)
+
+
+my_name = "hello"
+
+my_data = [["ori", "058", "rishon"],
+
+           ["dana", "069", "nes"]
+
+           ]
