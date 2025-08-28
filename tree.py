af1a3e20adce40da2c1f4b2a0fb254eb6f787f0b
yael-shmulevitch <yaelik.zhu@gmail.com>
2025-08-28 14:47:12 +0300

    tree

--- tree.py
+++ tree.py
@@ -1,2 +1,14 @@
 
+import consts
+import main
+import pygame
+
+def tree(num):
+    if num==1:
+        img = pygame.image.load( "assets/TREE1.png")
+    if num==2:
+        img = pygame.image.load("assets/TREE2.png")
+    if num==3:
+        img = pygame.image.load("assets/TREE3.png")
+    main.SCREEN.blit(img, (consts.SCREEN_WIDTH/2, consts.SCREEN_HEIGHT/2))
 
