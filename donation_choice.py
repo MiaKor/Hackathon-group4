94707e6bae71a4a5537c32933b77f92d24c3ba24
miashats-pixe <shklovski@>
2025-08-28 14:27:59 +0300

    don choice

--- donation_choice.py
+++ donation_choice.py
@@ -11,28 +11,24 @@ def donation_choice():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = main.get_font(100).render("Please choose your type of contribution", True, "#b68f40")
+        MENU_TEXT = main.get_font(70).render("Please choose your type of contribution", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
         donation_button = Button(image=pygame.image.load("assets/Play Rect.png"),
-                                pos=(400, 400),
-                                text_input="donation", font=main.get_font(75),
+                                pos=(300, 400),
+                                text_input="donation", font=main.get_font(40),
                                 base_color="#d7fcd4", hovering_color="White")
 
         volunteer_button = Button(image=pygame.image.load("assets/Play Rect.png"),
-                                pos=(400, 400),
-                                text_input="volunteer", font=main.get_font(75),
+                                pos=(640, 600),
+                                text_input="volunteer", font=main.get_font(40),
                                 base_color="#d7fcd4", hovering_color="White")
 
         contr_button = Button(image=pygame.image.load("assets/Play Rect.png"),
-                                pos=(400, 400),
-                                text_input="donate groceries", font=main.get_font(75),
+                                pos=(980, 400),
+                                text_input="donate groceries", font=main.get_font(40),
                                 base_color="#d7fcd4", hovering_color="White")
 
-        # OPTIONS_BUTTON = Button(
-        #     image=pygame.image.load("assets/Play Rect.png"), pos=(800, 400),
-        #     text_input="ORG", font=main.get_font(75), base_color="#d7fcd4",
-        #     hovering_color="White")
 
         main.SCREEN.blit(MENU_TEXT, MENU_RECT)
 
